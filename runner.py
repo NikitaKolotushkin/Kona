5d0119f24b504e732f53ff05665ff327524b21fd
NikitaKolotushkin <nikitaplay1000@gmail.com>
2023-10-03 13:04:11 +0300

    Добавлен менеджер запуска из flask_script

--- runner.py
+++ runner.py
@@ -2,12 +2,23 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from flask_script import Command, Manager, Shell
 
-from app import create_app
+from app import create_app, db
 from config import DevelopmentConfig, TestingConfig, ProductionConfig
+from manager_commands import Runner
 
 
 app = create_app(config_class=DevelopmentConfig)
+manager = Manager(app)
+
+
+def make_shell_context():
+    return dict(app=app, db=db)
+
+
+manager.add_command('shell', Shell(make_context=make_shell_context))
+manager.add_command('run_app', Runner(app))
 
 if __name__ == '__main__':
-    app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.config['DEBUG'], use_reloader=app.config['RELOADER'])
+    manager.run()
