507487f294b60460289b61d69ef7dda946596c9a
NikitaKolotushkin <nikitaplay1000@gmail.com>
2023-10-04 11:35:13 +0300

    Прописаны новые пути до баз данных

--- config.py
+++ config.py
@@ -24,14 +24,16 @@ class DevelopmentConfig(BaseConfig):
     DEVELOPMENT = True
     DEBUG = True
     RELOADER = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEVELOPMENT_DATABASE_URI') or 'sqlite:///' + os.path.join(app_dir, 'dev.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEVELOPMENT_DATABASE_URI') or 'sqlite:///' + os.path.join(app_dir, 'app/databases/dev.db')
 
 
 class TestingConfig(BaseConfig):
     DEBUG = False
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEVELOPMENT_DATABASE_URI') or 'sqlite:///' + os.path.join(app_dir, 'app/databases/test.db')
 
 
 class ProductionConfig(BaseConfig):
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEVELOPMENT_DATABASE_URI') or 'sqlite:///' + os.path.join(app_dir, 'app/databases/site.db')
