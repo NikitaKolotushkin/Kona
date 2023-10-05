82393cd32fac1ebde9a9fb83cf3eef6b125dfd51
NikitaKolotushkin <nikitaplay1000@gmail.com>
2023-10-05 15:44:33 +0300

    migrate.init_app([app] -> [app, db])

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@
 
 from flask import Flask
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from config import DevelopmentConfig, TestingConfig, ProductionConfig
@@ -10,6 +11,7 @@ from config import DevelopmentConfig, TestingConfig, ProductionConfig
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+migrate = Migrate()
 
 
 def create_app(config_class):
@@ -19,6 +21,8 @@ def create_app(config_class):
 
     db.init_app(app)
     login_manager.init_app(app)
+    login_manager.login_view = 'main.login'
+    migrate.init_app(app, db)
 
 
     from .main import main as main_blueprint
