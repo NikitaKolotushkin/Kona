49a27fc682bca5c1c19ba3de2be2c84d0d138e17
NikitaKolotushkin <nikitaplay1000@gmail.com>
2023-10-18 13:53:06 +0300

    Some docstrings & typehint

--- app/tools.py
+++ app/tools.py
@@ -4,7 +4,12 @@
 import re
 
 
-def validate_email(email):
+def validate_email(email) -> bool:
+    """ Проверяет валидность электронной почте по заданному регулярным выражением шаблону
+    
+    :rtype: bool
+    :return: True если email прошел валидацию, иначе - False
+    """
     pattern = '(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)'
 
     return bool(re.match(pattern, email))
