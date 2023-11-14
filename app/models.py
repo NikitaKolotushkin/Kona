49e924493060b9e3641224b48d3b934aed6a8c99
NikitaKolotushkin <nikitaplay1000@gmail.com>
2023-11-14 15:11:17 +0300

    Added some relationships

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@
 from app import db, login_manager
 
 from flask_login import LoginManager, UserMixin, login_required, login_user, current_user, logout_user
+from sqlalchemy.orm import relationship
 from werkzeug.security import check_password_hash, generate_password_hash
 
 
@@ -21,7 +22,8 @@ class User(db.Model, UserMixin):
     phone = db.Column(db.String(16))
     description = db.Column(db.Text(2048))
     photo = db.Column(db.LargeBinary(8000))
-    city_id = db.Column(db.String(64))
+    city_id = db.Column(db.Integer(), db.ForeignKey('cities.id'))
+    city = relationship("City")
     university = db.Column(db.String(255))
 
     def set_password(self, password):
@@ -40,7 +42,8 @@ class University(db.Model):
 
     id = db.Column(db.Integer(), primary_key=True, nullable=False, unique=True)
     name = db.Column(db.String(255), nullable=True, unique=True)
-    city = db.Column(db.String(128), nullable=False)
+    city_id = db.Column(db.Integer(), db.ForeignKey('cities.id'))
+    city = relationship("City")
 
     def __repr__(self):
         return f'<{self.id}:{self.name}>'
@@ -52,6 +55,8 @@ class City(db.Model):
 
     id = db.Column(db.Integer(), primary_key=True, nullable=False, unique=True)
     name = db.Column(db.String(128), nullable=False, unique=True)
+    users = relationship("User")
+    universities = relationship("University")
 
     def __repr__(self):
         return f'<{self.id}:{self.name}>'
