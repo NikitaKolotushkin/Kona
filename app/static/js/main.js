8c3185e23e2828afbbaded1422d6268f863d400e
NikitaKolotushkin <nikitaplay1000@gmail.com>
2024-02-16 10:01:06 +0300

    add: Base js script file

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -3,3 +3,18 @@
 // 
 // SOME CODE HERE
 // 
+
+
+//user_registration.html
+
+setInterval( function() {
+    var cursor = document.getElementById('cursor');
+
+    if (cursor.style.visibility === 'visible') {
+        cursor.style.visibility = 'hidden';
+    } else {
+        cursor.style.visibility = 'visible';
+    }
+}, 750)
+
+// changeCursorState();
