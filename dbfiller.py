abec0b35d43ffebc1434c16b612cff1f7da4e06c
NikitaKolotushkin <nikitaplay1000@gmail.com>
2024-03-20 10:39:24 +0300

    add: cities table filling in database

--- dbfiller.py
+++ dbfiller.py
@@ -3,29 +3,49 @@
 
 import codecs
 import json
-
-from app import db, engine
-from app.models import *
+import sqlite3
 
 
-def fill_cities() -> None:
-    with codecs.open('cities.json', 'r', 'utf_8_sig') as f:
-        data = [city for city in json.loads(f.read()).keys()]
-    return data
+class dbfiller:
+    def __init__(self) -> None:
+        self.connection = sqlite3.connect('app/databases/dev.db')
+        self.cursor = self.connection.cursor()
+
+    
+    def commit_changes(self) -> None:
+        self.connection.commit()
+        self.connection.close()
 
 
-def fill_universities() -> None:
-    with codecs.open('cities.json', 'r', 'utf_8_sig') as f:
-        data = sum([university for university in json.loads(f.read()).values() if university[0] != ''], [])
-    return data
+    def fill_cities(self) -> None:
+        with codecs.open('cities.json', 'r', 'utf_8_sig') as f:
+            data = [city for city in json.loads(f.read()).keys()]
+
+        for city in data:
+            self.cursor.execute('INSERT OR IGNORE INTO cities (name) VALUES (?)', (city, ))
+
+        self.commit_changes()
 
 
-def fill_interests() -> None:
-    return
+    def fill_universities(self) -> None:
+        with codecs.open('cities.json', 'r', 'utf_8_sig') as f:
+            data = sum([university for university in json.loads(f.read()).values() if university[0] != ''], [])
+
+        for university in data:
+            pass
+
+        self.commit_changes()
+
+
+    def fill_interests(self) -> None:
+        return
 
 
 def main():
-    print(fill_universities())
+    filler = dbfiller()
+    filler.fill_cities()
+    # filler.fill_universities()
+    # filler.fill_interests()
 
 
 if __name__ == '__main__':
