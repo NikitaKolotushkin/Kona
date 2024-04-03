0e6608322fd37a45088c6ff82df8b3b991fb457e
NikitaKolotushkin <nikitaplay1000@gmail.com>
2024-04-03 10:43:22 +0300

    Filling universities in .db file

--- dbfiller.py
+++ dbfiller.py
@@ -11,11 +11,6 @@ class dbfiller:
         self.connection = sqlite3.connect('app/databases/dev.db')
         self.cursor = self.connection.cursor()
 
-    
-    def commit_changes(self) -> None:
-        self.connection.commit()
-        self.connection.close()
-
 
     def fill_cities(self) -> None:
         with codecs.open('cities.json', 'r', 'utf_8_sig') as f:
@@ -28,24 +23,34 @@ class dbfiller:
 
 
     def fill_universities(self) -> None:
-        with codecs.open('cities.json', 'r', 'utf_8_sig') as f:
-            data = sum([university for university in json.loads(f.read()).values() if university[0] != ''], [])
+        with open('universities.txt', 'r') as f:
+            data = [x.rstrip() for x in f.readlines()]
 
         for university in data:
-            pass
+            self.cursor.execute('INSERT OR IGNORE INTO universities (name) VALUES (?)', (university, ))
 
         self.commit_changes()
 
 
     def fill_interests(self) -> None:
         return
+    
+
+    def commit_changes(self) -> None:
+        self.connection.commit()
+
+
+    def close_connection(self) -> None:
+        self.connection.close()
 
 
 def main():
     filler = dbfiller()
     filler.fill_cities()
-    # filler.fill_universities()
+    filler.fill_universities()
     # filler.fill_interests()
+
+    filler.close_connection()
 
 
 if __name__ == '__main__':
