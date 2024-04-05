9fc49486c5e0baf31b5adfc0fc894064ede2afb4
NikitaKolotushkin <nikitaplay1000@gmail.com>
2024-04-05 10:41:39 +0300

    Optimized functin arguments for user_profile

--- app/main/views.py
+++ app/main/views.py
@@ -147,6 +147,7 @@ def user_profile(user_tag):
     university_exists = user_data[12] is not None
 
     city = None
+    university = None
 
     query = [row for row in engine.connect().execute(
         select(Relations).where(or_(Relations.user_id == user_tag, Relations.friend_id == user_tag),
@@ -204,10 +205,12 @@ def user_profile(user_tag):
         profile_owner[table_keys[i]] = user_data[i]
 
     if city_exists:
-        city = [x for x in engine.connect().execute(select(City).where(City.id == profile_owner['city_id']))][0][1]
+        profile_owner['city'] = [x for x in engine.connect().execute(select(City).where(City.id == profile_owner['city_id']))][0][1]
 
-    return render_template('user_profile.html', title=f'Kona | {profile_owner["name"]} {profile_owner["surname"]}',
-                           city=city, city_exists=city_exists, university_exists=university_exists,
+    if university_exists:
+        profile_owner['university'] = [row for row in engine.connect().execute(select(University.name).where(University.id == profile_owner['university_id']))][0][0]
+
+    return render_template('user_profile.html', title=f'Kona | {profile_owner["name"]} {profile_owner["surname"]}', city_exists=city_exists, university_exists=university_exists,
                            pending_invite=pending_invite, accepted_invite=accepted_invite, sent_invite=sent_invite,
                            profile_owner=profile_owner, friend_count=friend_count)
 
