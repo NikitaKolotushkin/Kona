546b32ab51f0ab444297db005cf09b352c656a4c
NikitaKolotushkin <nikitaplay1000@gmail.com>
2024-07-31 19:18:53 +0400

    port changed to 5000

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class BaseConfig:
     UPLOAD_FOLDER: str = os.path.join(app_dir, 'uploads')
     JSON_AS_ASCII: bool = True
     HOST: str = os.environ.get('APP_HOST', '0.0.0.0')
-    PORT: int = int(os.environ.get('APP_PORT', 8080))
+    PORT: int = int(os.environ.get('APP_PORT', 5000))
     RELOADER = False
 
 
